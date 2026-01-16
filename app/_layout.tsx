aeeae708e28c38df9d21021e7e34b6665c0748b5
dilini <dilinichamikasilva@gmail.com>
2026-01-16 11:22:18 +0530

    feat: implement authentication screen with Firebase integration and create layout for navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import "../global.css";
+
+export default function RootLayout() {
+  const [user, setUser] = useState<User | null>(null);
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Listen for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    const inTabsGroup = segments[0] === "(tabs)";
+
+    if (!user && inTabsGroup) {
+      
+      router.replace("/");
+    } else if (user && !inTabsGroup) {
+      
+      router.replace("/(tabs)/map");
+    }
+  }, [user, segments]);
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+}
