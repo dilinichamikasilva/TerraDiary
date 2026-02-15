5845f9d56da98bfdc8fa760f881f282055d3ef50
dilini <dilinichamikasilva@gmail.com>
2026-02-16 00:01:55 +0530

    feat:changed firebase configurations

--- service/firebaseConfig.ts
+++ service/firebaseConfig.ts
@@ -1,24 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //@ts-ignore
 import { initializeAuth , getReactNativePersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCk_qST3oUrYvETaIOVHsSelkq2BJVBxgY",
-  authDomain: "terra-diary-2026.firebaseapp.com",
-  projectId: "terra-diary-2026",
-  storageBucket: "terra-diary-2026.firebasestorage.app",
-  messagingSenderId: "818258924002",
-  appId: "1:818258924002:web:a4f413ec8f1d77882669bb"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = initializeAuth(app , {
